a352ed1bc371a6ce111ccd4e4f7c05f8f883e6fc
Oluwatobi Adelabu <adelabutobi@gmail.com>
2022-06-30 15:42:18 -0700

    Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,18 @@
+/*
+ *  * File: 4-puts.c
+ *   */
+
 #include <stdio.h>
-int main(void){ puts("\"Programming is like building a multilingual puzzle"); return (0);}
+
+/**
+ *  * main - Prints "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line.
+ *    *
+ *     * Return: Always 0.
+ *      */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
