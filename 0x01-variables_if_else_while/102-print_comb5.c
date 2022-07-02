9db822996f2c8b691968c2b1074d390e6d26b654
Oluwatobi Adelabu <adelabutobi@gmail.com>
2022-07-01 20:48:32 -0700

    fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,11 +25,9 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-		
 			if (num1 == 98 && num2 == 99)
 				continue;
 
-		
 			putchar(',');
 			putchar(' ');
 		}
