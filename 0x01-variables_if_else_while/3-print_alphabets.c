971d0336d138ee4163ae64bc9ccaaecb017d1db5
Oluwatobi Adelabu <adelabutobi@gmail.com>
2022-07-01 21:32:46 -0700

    fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,15 +13,16 @@
 int main(void)
 {
 	char letter;
-	
+
 	for(letter = 'a'; letter <= 'z'; letter++)
-
+	{
 		putchar(letter);
-	
+	}
+
 	for(letter = 'A'; letter <= 'Z'; letter++)
-		
+	{
 		putchar(letter);
-
+	}
 		putchar('\n');
 
 		return (0);
