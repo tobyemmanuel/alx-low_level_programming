e6fe5ce1b436694b986ae399570ef8ce746a1a46
Oluwatobi Adelabu <adelabutobi@gmail.com>
2022-07-05 00:49:34 -0700

    Put Char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-putchar.c
+ * By: Tobi
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "ALX" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('A');
+	_putchar('L');
+	_putchar('X');
+	_putchar('\n');
+	return (0);
+}
