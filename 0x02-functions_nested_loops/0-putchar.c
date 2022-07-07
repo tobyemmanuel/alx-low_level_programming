045985c80b117cbc2555b8272379ba7c81a8683b
Oluwatobi Adelabu <adelabutobi@gmail.com>
2022-07-06 17:01:37 -0700

    one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,9 +13,14 @@
  */
 int main(void)
 {
-	_putchar('A');
-	_putchar('L');
-	_putchar('X');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
