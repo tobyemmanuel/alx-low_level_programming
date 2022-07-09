d5fd30259eff2614d2e6d2b48ea1454745eac9de
Oluwatobi Adelabu <adelabutobi@gmail.com>
2022-07-08 21:06:13 -0700

    advanced

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,17 @@
-/*
- * File: 4-print_most_numbers.c
- * Author: Tobi
- */
-
 #include "main.h"
-
 /**
- * print_most_numbers :- Prints the numbers from 0-9 except for 2 and 4.
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ * Return: 0-9, excluding 2 and 4, followed by new line
  */
 void print_most_numbers(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
-	{
-		if (num != 2 && num != 4)
-			_putchar((num % 10) + '0');
-	}
-
-	_putchar('\n');
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,23 @@
-/*
- * File: 5-more_numbers.c
- * Author: Tobi
- */
-
 #include "main.h"
-
 /**
- * more_numbers :- Prints the numbers 0-14 ten times.
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ * _putchar only 3 times
+ * Return: 0-14 10times followed by new line
  */
 void more_numbers(void)
 {
-	int num, count;
-
-	for (count = 0; count <= 9; count++)
-	{
-		for (num = 0; num <= 14; num++)
-		{
-			if (num > 9)
-				_putchar((num / 10) + '0');
-			_putchar((num % 10) + '0');
-		}
-		_putchar('\n');
-	}
+int i;
+int b;
+for (i = 0; i <= 9; i++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b > 9)
+{
+_putchar('1');
+}
+_putchar((b % 10) + '0');
+}
+_putchar('\n');
+}
 }
