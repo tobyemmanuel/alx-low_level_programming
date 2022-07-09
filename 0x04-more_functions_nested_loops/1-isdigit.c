7a3752c101c6f5d7beca3858e6eb5569a30d432a
Oluwatobi Adelabu <adelabutobi@gmail.com>
2022-07-08 20:51:57 -0700

    three

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,21 @@
+/*
+ * Filename: 1-isdigit.c
+ * Author: Tobi
+ */
+
 #include "main.h"
+
 /**
- * _isdigit function checks for a digit 0-9
- * @c: input
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - Checks for a digit (0-9).
+ * @c: The number to be checked.
+ *
+ * Return 1 if the number is a digit, 0 otherwise.
  */
 int _isdigit(int c)
 {
-if ((c >= 48) && (c <= 57))
-return (1);
-else
-return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,12 @@
-#include "holberton.h"
+/*
+ * File: 4-print_most_numbers.c
+ * Author: Tobi
+ */
+
+#include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ * print_most_numbers :- Prints the numbers from 0-9 except for 2 and 4.
  */
 void print_most_numbers(void)
 {
